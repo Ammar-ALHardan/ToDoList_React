84653d9fad2e15a4d45a2be8504465f1bb407324
Ammar.Hardan <Ammar.hardan@ihorizons.com>
2025-07-15 15:20:16 +0300

    added project name in aside menu when added

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,14 @@ function App() {
 
   function handleAddProject(projectData) {
     setprojectState(prevState => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       }
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       }
     });
@@ -41,7 +43,9 @@ function App() {
   }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar 
+      onStartAddProject={handleStartAddProject} 
+      projects={projectState.projects} />
       {content}
     </main>
   );
--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,11 +1,21 @@
 import Button from "./Button";
 
-export default function ProjectsSidebar({onStartAddProject}) {
+export default function ProjectsSidebar({onStartAddProject, projects}) {
     return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-slate-50 md:w-72 rounded-r-xl">
         <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
         <Button onClick={onStartAddProject}>+ Add Project</Button>
         <ul>
+            {projects.map((project) => (
+                <li key={project.id} className="mt-8">
+                    <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+                        {project.title}
+                    </button>
+                </li>
+            ))}
+            {projects.length === 0 && (
+                <li className="text-stone-400 mt-8">No projects available</li>
+            )}
         </ul>   
     </aside>
     );
